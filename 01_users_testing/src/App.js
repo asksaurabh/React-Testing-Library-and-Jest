b4150a179dfc66b7bb928063cf0e80c32608e4c2
Saurabh Kumar <asksaurabh101@gmail.com>
2023-03-03 12:54:35 +0530

    Set users state and show Form and List components

--- 01_users_testing/src/App.js
+++ 01_users_testing/src/App.js
@@ -1,10 +1,20 @@
+import { useState } from 'react';
 import './App.css';
 import UserForm from './components/UserForm';
+import UserList from './components/UserList';
 
 function App() {
+  const [users, setUsers] = useState([]);
+
+  const handleUserAdd = (user) => {
+    setUsers([...users, user]);
+  };
+
   return (
     <div>
-      <UserForm />
+      <UserForm onUserAdd={handleUserAdd} />
+      <hr />
+      <UserList users={users} />
     </div>
   );
 }
