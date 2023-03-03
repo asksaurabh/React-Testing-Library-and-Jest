8b3fd2ad0897e5384922c1d8b35074dc2e2f80bb
Saurabh Kumar <asksaurabh101@gmail.com>
2023-03-03 15:25:09 +0530

    Add test to simulate user submitting a UserForm

--- 01_users_testing/src/UserForm.test.js
+++ 01_users_testing/src/UserForm.test.js
@@ -11,3 +11,36 @@ test('UserForm shows two inputs and a button', () => {
   expect(inputs).toHaveLength(2);
   expect(button).toBeInTheDocument();
 });
+
+test('UserForm calls on onUserAdd when the from is submitted', () => {
+  // Not the best Implementation
+  const argList = [];
+  const callback = (...args) => {
+    argList.push(args);
+  };
+  render(<UserForm onUserAdd={callback} />);
+
+  // Find the two inputs
+  const [nameInput, emailInput] = screen.getAllByRole('textbox');
+
+  // Simulate typing a name
+  user.click(nameInput);
+  user.keyboard('michael');
+
+  // Simulate typing an email
+  user.click(emailInput);
+  user.keyboard('michael@gamil.com');
+
+  // Find the button
+  const button = screen.getByRole('button');
+
+  // Simulate clicking the button
+  user.click(button);
+
+  // Assertion to make sure 'onUserAdd' gets called with email/name
+  expect(argList).toHaveLength(1);
+  expect(argList[0][0]).toEqual({
+    name: 'michael',
+    email: 'michael@gamil.com',
+  });
+});
