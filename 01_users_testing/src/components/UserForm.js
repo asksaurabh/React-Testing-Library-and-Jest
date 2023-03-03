a38b2191990bb738d7e7288843e45c0e7b83864a
Saurabh Kumar <asksaurabh101@gmail.com>
2023-03-03 12:55:05 +0530

    Add UserForm component

--- 01_users_testing/src/components/UserForm.js
+++ 01_users_testing/src/components/UserForm.js
@@ -1,13 +1,28 @@
-function UserForm() {
+import { useState } from 'react';
+
+function UserForm({ onUserAdd }) {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+    onUserAdd({ name, email });
+    setName('');
+    setEmail('');
+  };
+
   return (
-    <form>
+    <form onSubmit={handleFormSubmit}>
       <div>
         <label>Name</label>
-        <input />
+        <input value={name} onChange={(event) => setName(event.target.value)} />
       </div>
       <div>
         <label>Email</label>
-        <input />
+        <input
+          value={email}
+          onChange={(event) => setEmail(event.target.value)}
+        />
       </div>
       <button>Add User</button>
     </form>
