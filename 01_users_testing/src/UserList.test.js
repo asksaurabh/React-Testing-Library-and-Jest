d16bdbab39f63ac3c32185c2a538869575310b51
Saurabh Kumar <asksaurabh101@gmail.com>
2023-03-04 09:38:50 +0530

    Refactor duplicate code inside test file: initial setup

--- 01_users_testing/src/UserList.test.js
+++ 01_users_testing/src/UserList.test.js
@@ -1,7 +1,7 @@
 import { render, screen, within } from '@testing-library/react';
 import UserList from './components/UserList';
 
-test('Render one row per user', () => {
+function renderComponent() {
   const users = [
     {
       name: 'Saurabh',
@@ -14,25 +14,19 @@ test('Render one row per user', () => {
   ];
 
   render(<UserList users={users} />);
+  return {
+    users,
+  };
+}
 
+test('Render one row per user', () => {
+  renderComponent();
   const rows = within(screen.getByTestId('users')).getAllByRole('row');
-
   expect(rows).toHaveLength(2);
 });
 
 test('Render the email and name of each user', () => {
-  const users = [
-    {
-      name: 'Saurabh',
-      email: 'saurabh@gmail.com',
-    },
-    {
-      name: 'Rishabh',
-      email: 'rishabh@gmail.com',
-    },
-  ];
-
-  render(<UserList users={users} />);
+  const { users } = renderComponent();
 
   for (let user of users) {
     const name = screen.getByRole('cell', { name: user.name });
