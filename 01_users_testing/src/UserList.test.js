4856e675fe3abedc1b86a3ef9dd820bc99dd7bdc
Saurabh Kumar <asksaurabh101@gmail.com>
2023-03-04 09:31:44 +0530

    Add tests to check if name and email is rendered on screen or not

--- 01_users_testing/src/UserList.test.js
+++ 01_users_testing/src/UserList.test.js
@@ -13,13 +13,32 @@ test('Render one row per user', () => {
     },
   ];
 
-  const { container } = render(<UserList users={users} />);
+  render(<UserList users={users} />);
 
-  // const rows = within(screen.getByTestId('users')).getAllByRole('row');
-  // eslint-disable-next-line
-  const rows = container.querySelectorAll('tbody tr');
+  const rows = within(screen.getByTestId('users')).getAllByRole('row');
 
   expect(rows).toHaveLength(2);
 });
 
-// test('Render the email and name of each user', () => {});
+test('Render the email and name of each user', () => {
+  const users = [
+    {
+      name: 'Saurabh',
+      email: 'saurabh@gmail.com',
+    },
+    {
+      name: 'Rishabh',
+      email: 'rishabh@gmail.com',
+    },
+  ];
+
+  render(<UserList users={users} />);
+
+  for (let user of users) {
+    const name = screen.getByRole('cell', { name: user.name });
+    const email = screen.getByRole('cell', { name: user.email });
+
+    expect(name).toBeInTheDocument();
+    expect(email).toBeInTheDocument();
+  }
+});
